e5ce5dcfa0394e656d37a3a6f3b01102154aea9c
Michael Lee <mlee1974@gmail.com>
2014-10-15 17:17:29 -0700

    update to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_setting = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
